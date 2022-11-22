8c41efea49dc92f32de719d8c8037ce101e83d1c
fistonhn <fistonhn@gmail.com>
2022-11-22 21:35:38 +0200

    upload many pictures

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const postController = require("../controllers/post");
-const Validator = require("../middlewares/validator");
 const verifyUserAuthToken = require("../middlewares/verifyToken");
 const upload = require("../middlewares/uploads");
 
@@ -8,8 +7,8 @@ const upload = require("../middlewares/uploads");
 const router = express.Router();
 
 router.post("/signup",  postController.createUser);
-router.post("/login", [ Validator.loginValidator ], postController.login);
-router.post("/createPost", [ upload, Validator.postValidator, verifyUserAuthToken ], postController.createPost);
+router.post("/login", postController.login);
+router.post("/createPost", [ upload, verifyUserAuthToken ], postController.createPost);
 
 router.get("/getAllPosts", [ verifyUserAuthToken ], postController.getAllPosts);
 router.get("/getOnePost/:id", [ verifyUserAuthToken ], postController.getOnePost);
